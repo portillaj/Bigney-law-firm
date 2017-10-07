ac98202a0b8769e596547cbde18c78e54199d9eb
Jason Portilla <jason.portilla85@gmail.com>
2017-10-07 22:54:58 +0000

    working on home page for website

---
+++ src/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Bootstrap 4 Starter Pack</title>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+<div class="home-header">
+  <div class="dark-overlay"></div>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+  <div class="navbar-brand">
+   <h3 class="mt-2"> 
+   <span>
+     <i class="fa fa-balance-scale" aria-hidden="true"></i></span>
+     David Bigney
+    </h3>
+  </div>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Our Firm</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Area of Practice</a>
+      </li>
+    </ul>
+      <button class="btn btn-outline-success my-1 my-sm-0" type="submit">Free Consultation</button>
+  </div>
+  </div>
+</nav>
+
+<div class="header-content">
+<div class="container">
+<div class="row">
+  <div class="col-lg-7">
+    <h1 class="display-5 pt-5 text-light">Defending Your Rights,<br> Is Our #1 Priority</h1>
+    <p>Your Defense In Criminal Charges & Personal Injury Cases in Florida. 
+    No case is too large or too small for The Bigney Law Firm</p>
+    <div class="d-flex flex-row call-to-action">
+      <button class="btn btn-outline-light">Free Consultation</button>
+      <h4 class="ml-4 pt-2 learn">Learn More</h4>
+    </div>
+  </div>
+  
+  <div class="col-lg-5">
+    <img src="img/attorney.png" class="img img-fluid" />
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
